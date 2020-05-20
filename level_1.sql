c3a498836b7c5db674f374ff367b5dc36250ca67
Abraham Jones <56662623+abejones26@users.noreply.github.com>
2020-05-20 13:40:43 -0500

    Add description of datatables

--- level_1.sql
+++ level_1.sql
@@ -1,3 +1,62 @@
+-- Privacy of Randa Accessories, I am only able to show you the frame work of this database and datatable.
+-- See screenshot for visualizations.
+
+-- Datatable and descriptions
+    -- ALP: Report for Inventory Management and Warehouse
+        -- ID: Key
+        -- Cut Number: Purchase order number
+        -- ALP: Alpha size label
+
+    -- Buying Group: Report of Sales, Forecasting, and Inventory Management
+        -- ID: Key
+        -- Style: Product style serial number
+        -- Color: Product color code
+        -- Buying Groups: Sales customer buying groups and ID of buying groups
+
+    -- CORPO2WK is the report I am re-developing.
+    -- CORPO2WK: Original 30 years old version of Randa's high level global product production core report.
+        -- ID: Key
+        -- Company: Code to determine which category a product belongs such as Belt, Shoe or luggage.
+        -- Division: Code to determine which location of the company category belongs
+        -- Core Code: High level category of product style and color that determines the amount of inventory productions and purchase orders.
+        -- Sort Supplier: Suppliers' ID number
+        -- Supplier Number: Suppliers' ID number
+        -- Style: Product style number
+        -- Color: Product color code
+        -- Status: Empty
+        -- Tier: Empty
+        -- Effective Date: Empty
+        -- Beginning Balance: Purchase Order amount at the beginning of sale
+        -- Open Balance: Purchase Order amount after customer order picked
+        -- Shipped Quantity: Amount of pieces shipped
+        -- Heading Month 1 to Month 6: Placeholder of month 1 to 6
+        -- Forecast Month 1 to Month 8: Product Orders forecast for month 1 to Month 8
+        -- Receipts Month 1 to Month 6: Amount of pieces being received at the warehouse for month 1 to Month 6
+        -- Balance Month 1 to Month 6: Amount of pieces after shipping and forecast, divided by next month forecast
+
+    -- Diver: Report for Supply Planners, Inventory Management, Sourcing, and Sales
+        -- ID: Key
+        -- Style: Product style serial number
+        -- Plant Code: Factory name and name code
+        -- Inventory Type: Product caterogy
+        -- Royalty Code: Customer name and name code
+
+    -- Due to report size i'll only give description of featured columns
+    -- WMAWIPSE: Product report for Warehouse, Supply Planner, Inventory Management, Sourcing, Sales,Forecasting and Merchadising
+        -- ID:Key
+        -- Shop Code table #26: Factory name code
+        -- Ship Via Table #01: Shipping method freight, air or boat.
+        -- Cut number: Purchase order number
+        -- Cut sequence number: Split of purchase order number
+        -- Shade code - table #45: Determine the location of products in warehouse
+        -- Open Qty: Purchase Order amount after customer order picked
+        -- Original Ship date: Ship date when purchase order is cut
+        -- Requested Ship date: Date change requested by factory or Randa company date change
+        -- Shipment date: Date order shipped from factory
+        -- Warehouse Date: Date order arrived at the warehouse
+        -- Revised reason code: Shipping delayed reason code
+        -- Comments: company comments about products or order
+
 SELECT "" AS Duplicate,
     [CORP02WK]![Style] & [CORP02WK]![Color] AS [KEY], 
     CORP02WK.[Core Code], 
